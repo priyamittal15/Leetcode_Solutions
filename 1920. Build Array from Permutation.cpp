c3f314e4e9129465001a70eb06c496cfafb0fa6a
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-01-27 23:09:59 +0530

    Add files via upload

---
+++ 1920. Build Array from Permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>num1(n);
+       
+        for(int i=0;i<n;i++){
+            num1[i]=nums[nums[i]];
+        }
+        return (num1);
+    }
+};

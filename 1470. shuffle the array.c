f80c89cd486c877b19bde7096998dfa9f6bc48d1
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-03-14 23:03:17 +0530

    Add files via upload

---
+++ 1470. shuffle the array.c
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X=0;
+        for(int i=0; i<operations.size();i++){
+            if(operations[i]=="--X" or operations[i]=="X--"){
+                X--;
+            }
+            else if(operations[i]=="++X" or  operations[i]== "X++" ){
+                X++;
+            }
+
+
+
+        }
+        return X;
+    }
+};

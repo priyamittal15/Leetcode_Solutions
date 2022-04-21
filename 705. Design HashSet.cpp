b8752e5fcd468891a5743757b594c7619d746559
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-04-21 11:50:53 +0530

    Add files via upload

---
+++ 705. Design HashSet.cpp
@@ -0,0 +1,41 @@
+class MyHashSet {
+public:
+    int MOD;
+    vector<vector<int>> stuff;
+    MyHashSet() {
+        MOD = 1000;
+        stuff.resize(MOD);
+    }
+    
+    void add(int key) {
+        int myKey = key % MOD;
+        for(int x : stuff[myKey]) {
+            if(x == key) return;
+        }
+        stuff[myKey].push_back(key);
+    }
+    
+    void remove(int key) {
+        int myKey = key % MOD;
+        auto it = find(stuff[myKey].begin(), stuff[myKey].end(), key);
+        if(it != stuff[myKey].end()) {
+            stuff[myKey].erase(it);
+        }       
+    }
+    
+    bool contains(int key) {
+        nt myKey = key % MOD;
+        auto it = find (stuff[myKey].begin(), stuff[myKey].end(), key);
+        return it != stuff[myKey].end();
+
+    }
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */

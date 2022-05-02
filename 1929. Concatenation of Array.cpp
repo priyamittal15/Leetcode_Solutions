7cf350b6773ccd05841375fc27c80a9452dfcf7f
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-05-02 16:48:31 +0530

    Update 1929. Concatenation of Array.cpp

--- 1929. Concatenation of Array.cpp
+++ 1929. Concatenation of Array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
+    vector<int> getconcatenation(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n;i++){
             nums.push_back(nums[i]);

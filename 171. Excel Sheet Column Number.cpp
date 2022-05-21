30225138d5e856eefeeff072ddfc133b0a24edc3
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-05-21 09:32:39 +0530

    Add files via upload

--- 171. Excel Sheet Column Number.cpp
+++ 171. Excel Sheet Column Number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int TitleToNumber(string columnTitle) {
+    int titleToNumber(string columnTitle) {
         int result = 0;
     for (int i = 0; i < columnTitle.size(); result = result * 26 + (columnTitle.at(i) - 'A' + 1), i++);
     return result;

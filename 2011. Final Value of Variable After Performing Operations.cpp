b6a921363e636b330fe83b0a4b7660cdbe5dab81
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-05-26 11:47:55 +0530

    Update 2011. Final Value of Variable After Performing Operations.cpp

--- 2011. Final Value of Variable After Performing Operations.cpp
+++ 2011. Final Value of Variable After Performing Operations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int finalValueAfterOperations(vector<string>& operations) {
+    int FinalValueAfterOperations(vector<string>& operations) {
         int X=0;
         for(int i=0; i<operations.size();i++){
             if(operations[i]=="--X" or operations[i]=="X--"){

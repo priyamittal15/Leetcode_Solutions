22673b66d7a69c4ecf9dc493074c58551a1baf38
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-06-24 11:32:30 +0530

    Add files via upload

---
+++ 138. Copy List with Random Pointer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        RandomListNode *newHead, *l1, *l2;
+    if (head == NULL) return NULL;
+    for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+        l2 = new RandomListNode(l1->label);
+        l2->next = l1->next;
+        l1->next = l2;
+    }
+        
+    newHead = head->next;
+    for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+        if (l1->random != NULL) l1->next->random = l1->random->next;
+    }
+        
+    for (l1 = head; l1 != NULL; l1 = l1->next) {
+        l2 = l1->next;
+        l1->next = l2->next;
+        if (l2->next != NULL) l2->next = l2->next->next;
+    }
+
+    return newHead;
+    }
+};
+

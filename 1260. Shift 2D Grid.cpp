6c9613a512cb806da271b773e0c42ed7cc07566e
PRIYA MITTAL <76867203+priyamittal15@users.noreply.github.com>
2022-07-31 10:33:58 +0530

    Update 1260. Shift 2D Grid.cpp

--- 1260. Shift 2D Grid.cpp
+++ 1260. Shift 2D Grid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int n=grid.size();
-        int m=grid[0].size()
+        int m=grid[0].size();
         vector<vector<int>> ans(n,vector<int>(m));
         
         for(int i=0;i<n;i++){
